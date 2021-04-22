4d1a955dad1f6bb1a863d04c80179c682525b885
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-22 16:06:42 -0500

    Define columns in 'Tag.js'

--- models/Tag.js
+++ models/Tag.js
@@ -5,8 +5,16 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+  {// define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
