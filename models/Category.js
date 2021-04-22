fcb02f384bb828fd5ed8b9f273ceef566b84bffc
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-22 15:59:41 -0500

    Define columns in 'Category.js'

--- models/Category.js
+++ models/Category.js
@@ -5,8 +5,17 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
+  {// define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
