93a2897efdf38e1997cbb909353be79649419031
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-29 16:49:08 -0500

    Complete GET routes w edge cases

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  // find all products + associated Category and Tag data
+  try {
+    const productInfo = await Product.findAll({ include: [ Category, Tag ] });
+    res.status(200).json(productInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id` + associated Category and Tag data
+  try {
+    const productInfo = await Product.findByPk(req.params.id, { include: [Category, Tag] });
+
+    if (!productInfo) {
+      res.status(404).json({ message: 'Product not found! No products are listed by this id.' });
+      return;
+    }
+
+    res.status(200).json(productInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +65,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,7 +106,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
